2be3ee42df81b2ffb4c931af22eefe66a383284a
thijsBoet <m.boet@chello.nl>
2023-01-19 16:02:45 +0100

    Finished discover page

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,23 +1,21 @@
-import { useState } from 'react';
-
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
-import { logo } from '../assets';
 
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const handlePauseClick = () => {
-    dispatch(playPause(false));
-  };
+	const handlePauseClick = () => {
+		dispatch(playPause(false));
+	};
 
-  const handlePlayClick = song => {
-    dispatch(setActiveSong({ song, data, i }));
-    dispatch(playPause(true));
-  };
+	const handlePlayClick = () => {
+		dispatch(setActiveSong({ song, data, i }));
+		dispatch(playPause(true));
+	};
 
 	return (
 		<div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
@@ -30,15 +28,20 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
 					}`}
 				>
 					<PlayPause
-            isPlaying={isPlaying}
-            activeSong={activeSong}
+						isPlaying={isPlaying}
+						activeSong={activeSong}
 						song={song}
 						handlePause={handlePauseClick}
-            handlePlay={handlePlayClick}
+						handlePlay={handlePlayClick}
 					/>
 				</div>
-				<img alt='song_img' src={song?.images?.coverart} />
+				<img
+					alt='song_img'
+					src={song.images?.coverart}
+					className='w-full h-full rounded-lg'
+				/>
 			</div>
+
 			<div className='mt-4 flex flex-col'>
 				<p className='font-semibold text-lg text-white truncate'>
 					<Link to={`/songs/${song?.key}`}>{song.title}</Link>
@@ -46,12 +49,12 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
 				<p className='text-sm truncate text-gray-300 mt-1'>
 					<Link
 						to={
-							song?.artists
-								? `/artist/${song?.artists[0]?.adamid}`
+							song.artists
+								? `/artists/${song?.artists[0]?.adamid}`
 								: '/top-artists'
 						}
 					>
-						{song?.subtitle}
+						{song.subtitle}
 					</Link>
 				</p>
 			</div>
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -20,6 +20,7 @@ const Discover = () => {
 		<div className='flex flex-col'>
 			<div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
 				<h2 className='font-bold text-3xl text-white text-left'>Discover</h2>
+
 				<select
 					name='select-genre'
 					id='select-genre'
